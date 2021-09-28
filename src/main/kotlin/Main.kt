b5b13d1b892c552f7c6fe323e32bbd089d6c109a
Vincent Cubit <vinniecubit@yahoo.com>
2021-09-28 18:03:07 -0500

    Updated

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,7 +3,10 @@ import java.text.DecimalFormat
 
 fun main() {
 
+    // User's total
     var total = 0F
+
+    // Expenses
     val tax = .025
     val tip = .175
     val extraCharges = tax + tip
@@ -11,12 +14,17 @@ fun main() {
 
     do {
 
+        // Getting input
         println("Enter the cost of an item. Type 0 to finish. :  ")
         val input = readLine()!!.toFloat()
+
+        // Adding to total
         total += input
 
 
     } while (input != 0F)
+
+    // Final total after expenses
     val finalTotal = total - ( total * extraCharges )
     println("After a 2.5% tax and a 17.5% tip, your total will be " + String.format("$%.2f", finalTotal) + ". Thanks!")
 
